ce162da909624b77b5d625c08493b935da49c5e7
Henrique Finatti <finatti.henrique@gmail.com>
2026-03-14 16:16:46 -0300

    Adicionando lista de nomes e lendo ela no servidor

--- codigo-csharp/Cliente/cliente.cs
+++ codigo-csharp/Cliente/cliente.cs
@@ -1,9 +1,27 @@
 using System;
-
+using System.Threading;
+using NetMQ;
+using NetMQ.Sockets;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        using (var client = new RequestSocket())
+        {
+            client.Connect("tcp://servidor-csharp:5555");
+            int i = 0;
+
+            while (true)
+            {
+                Console.Write($"Mensagem {i}: ");
+
+                client.SendFrame("Hello");
+                string message = client.ReceiveFrameString();
+                Console.WriteLine(message);
+                i++;
+
+                Thread.Sleep(1000);
+            }
+        }
     }
 }
--- codigo-csharp/Servidor/servidor.cs
+++ codigo-csharp/Servidor/servidor.cs
@@ -1,9 +1,53 @@
 using System;
-
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading;
+using System.Collections.Generic;
+using NetMQ;
+using NetMQ.Sockets;
 class Program
 {
+    static List<string> BuildNamesList()
+    {
+        string path = "dbload.txt";
+        List<string> nomesCarregados = new List<string>();
+
+        try
+        {
+            string conteudo = File.ReadAllText(path);
+            string[] nomes = conteudo.Split(",");
+
+            nomesCarregados = nomes.Select(n => n.Trim()).OrderBy(x => Random.Shared.Next()).Take(9).ToList();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao ler o arquivo: {ex.Message}");
+            return nomesCarregados;
+        }
+
+        return nomesCarregados;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        List<string> nomesCarregados = BuildNamesList();
+
+        foreach (string nome in nomesCarregados)
+        {
+            Console.WriteLine(nome);
+        }
+        using (var server = new ResponseSocket())
+        {
+            int i = 0;
+            server.Bind("tcp://*:5555");
+            while (true)
+            {
+                string message = server.ReceiveFrameString();
+                string name = nomesCarregados[i];
+                server.SendFrame(name);
+
+                i = i < 9 ? i = i + 1 : 8;
+            }
+        }
     }
 }
