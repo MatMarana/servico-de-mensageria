05670c562767d5f257b4dcfdb082d7ee95586af5
Mateus Marana <assumateus@gmail.com>
2026-03-19 16:12:41 -0300

    feat[adicionado protobuff para serialização das mensagens e conectado ao broker]

--- codigo-ruby/cliente.rb
+++ codigo-ruby/cliente.rb
@@ -1,13 +1,26 @@
+STDOUT.sync = true
+
 require "ffi-rzmq"
+require "google/protobuf"
 require "time"
+
+require_relative "ruby-proto_pb.rb"
+
+nomes_login = ["Ale", "Gabriel", "Giovanni", "Henrique", "Kawan", "Leo", "Mateus", "Pedro", "Roberto", "Tiago"]
+nomes_canais = ["Sistemas Distribuidos", "Jogos", "IA", "Engenharia de Software", "Gestão de Projetos", "TCC"]
 
 context = ZMQ::Context.new
 
 socket = context.socket(ZMQ::REQ)
-socket.connect("tcp://servidor-ruby:5555")
-loop do
-  socket.send_string("Tiago")
-  reply = ""
-  socket.recv_string(reply)
-  puts "Resposta #{reply}"
+socket.connect("tcp://broker:5555")
+
+nomes_login.each do |nome|
+  parts = []
+  request = MensagemProto::NomeLogin.new("nome": nome)
+  request_bin = MensagemProto::NomeLogin.encode(request)
+  socket.send_string(request_bin)
+  reply_bin = ""
+  socket.recv_strings(parts)
+  reply = MensagemProto::ResponseLogin.decode(parts.last)
+  puts "Resposta #{reply.resposta}"
 end
--- codigo-ruby/servidor.rb
+++ codigo-ruby/servidor.rb
@@ -1,14 +1,30 @@
+STDOUT.sync = true
+
 require "ffi-rzmq"
+require "google/protobuf"
 require "time"
 
-context = ZMQ::Context.new
-socket = context.socket(ZMQ::REP)
+require_relative "ruby-proto_pb.rb"
 
-socket.bind("tcp://*:5555")
+lista_logados = []
+
+context = ZMQ::Context.new
+socket = context.socket(ZMQ::DEALER)
+
+socket.connect("tcp://broker:5556")
 
 loop do 
-  mensagem = ""
-  socket.recv_string(mensagem)
-  puts "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} | Bem vindo #{mensagem}"
-  socket.send_string("OK")
+  parts = []
+  socket.recv_strings(parts)
+
+  mensagem_bin = parts.last
+  mensagem = MensagemProto::NomeLogin.decode(mensagem_bin)
+
+  puts "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} | Bem vindo #{mensagem.nome}"
+
+  mensagem = "OK"
+  reply = MensagemProto::ResponseLogin.new("resposta": mensagem)
+  reply_bin = MensagemProto::ResponseLogin.encode(reply)
+  parts[-1] = reply_bin
+  socket.send_strings(parts)
 end
