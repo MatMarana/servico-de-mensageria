b85a3c348aed57db86bd1f8eb16fbec27631c8c3
Tiago Fagundes dos Santos <tiagofagundesdossantos@gmail.com>
2026-05-17 15:39:26 -0300

    Ajusatando alguns pontos

--- codigo-python/cliente.py
+++ codigo-python/cliente.py
@@ -39,10 +39,10 @@ def extrair_canais(texto:str):
 contador_logico = 0
 
 # Função para incrementar o relógio lógico
-def incrementar_relogio():
-    global contador_logico
-    contador_logico += 1
-    return contador_logico
+# def incrementar_relogio():
+#     # global contador_logico
+#     contador_logico += 1
+#     return contador_logico
 
 # Função para atualizar o relógio lógico ao receber uma mensagem
 def atualizar_relogio(recebido):
@@ -56,6 +56,14 @@ def extrair_relogio(resposta):
         partes = resposta.split("|")
         relogio = int(partes[-1].split(":")[-1])
         return relogio
+    except:
+        return 0
+    
+def extrair_resposta(resposta):
+    try:
+        partes = resposta.split("|")
+        resposta_servidor = partes[0]
+        return resposta_servidor
     except:
         return 0
 #============================================
@@ -82,7 +90,7 @@ subscriber = True
 
 if not logado:
     for usuario in nomes:
-        incrementar_relogio()
+        contador_logico += 1
         mensagem = f"login|{usuario}|{datetime.now()}|relogio:{contador_logico}"
         mensagem = mensagem.strip().lower()
         print(f"{mensagem}", flush=True)
@@ -91,18 +99,19 @@ if not logado:
         resposta_bin = socket.recv()
         resposta = msgpack.unpackb(resposta_bin, raw=False)
         relogio_recebido = extrair_relogio(resposta)
+        resposta_servidor = extrair_resposta(resposta)
         atualizar_relogio(relogio_recebido)
         
-        if resposta == "login":
+        if resposta_servidor == "login":
             logado = True
             break
-        elif resposta == "erro":
+        elif resposta_servidor == "erro":
             ...
 
 
 if logado and not canal_bool:
     for canal in canais:
-        incrementar_relogio()
+        contador_logico += 1
         mensagem = f"canais|{canal}|{datetime.now()}|relogio:{contador_logico}"
         mensagem = mensagem.strip().lower()
         print(f"{mensagem}", flush=True)
@@ -111,16 +120,17 @@ if logado and not canal_bool:
         resposta_bin = socket.recv()
         resposta = msgpack.unpackb(resposta_bin, raw=False)
         relogio_recebido = extrair_relogio(resposta)
+        resposta_servidor = extrair_resposta(resposta)
         atualizar_relogio(relogio_recebido)
-        if resposta == "erro":
+        if resposta_servidor == "erro":
             canal_bool = True
             break
-        elif resposta == "sucesso":
+        elif resposta_servidor == "sucesso":
             ...
 
 if canal_bool and listar:
     while(listar):
-        incrementar_relogio()
+        contador_logico += 1
         mensagem = f"listar||{datetime.now()}|relogio:{contador_logico}"
         mensagem = mensagem.strip().lower()
         print(f"{mensagem}", flush=True)
@@ -129,6 +139,7 @@ if canal_bool and listar:
         resposta_bin = socket.recv()
         resposta = msgpack.unpackb(resposta_bin, raw=False)
         relogio_recebido = extrair_relogio(resposta)
+        resposta_servidor = extrair_resposta(resposta)
         atualizar_relogio(relogio_recebido)
         listar = False
 
@@ -143,7 +154,7 @@ if not listar and subscriber:
     while(subscriber):
         indice = random.randint(0, 2)
         i += 1
-        incrementar_relogio()
+        contador_logico += 1
         mensagem = f"canal|{canais_aleatorios[indice]}-teste{i}|{datetime.now()}|relogio:{contador_logico}"
         mensagem = mensagem.strip().lower()
         print(f"{mensagem}", flush=True)
@@ -152,6 +163,7 @@ if not listar and subscriber:
         resposta_bin = socket.recv()
         resposta = msgpack.unpackb(resposta_bin, raw=False)
         relogio_recebido = extrair_relogio(resposta)
+        resposta_servidor = extrair_resposta(resposta)
         atualizar_relogio(relogio_recebido)
         topico = sub.recv_string()
         time.sleep(1)
